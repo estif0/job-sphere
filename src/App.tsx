8ade6bbe716b9f878175ebf8fc43cadfca4a639f
Estifanose Sahilu <estifanoswork@gmail.com>
2025-02-03 17:59:29 +0300

    feat - Initailize project files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-8xl text-blue-700">Test Mike 1 2 3</h1>
+        </>
+    );
+}
+
+export default App;
