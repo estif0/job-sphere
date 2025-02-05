a42e856e2dfb063d033d049e6447fa5349c8f327
Estifanose Sahilu <estifanoswork@gmail.com>
2025-02-05 19:49:10 +0300

    feat - Add styling to job component

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -10,18 +10,22 @@ interface JobProps {
 export const Job: FC<JobProps> = ({ job }) => {
     return (
         // background of the job card
-        <div className="mb-6 flex flex-col p-4 bg-white shadow-lg rounded-lg">
+        <div className="mb-6 flex flex-col p-4 bg-white shadow-lg rounded-lg border-1 border-gray-100">
             <div className="flex justify-between space-x-2">
-                <img src={job.logo} alt="" className="h-20" />
+                <img src={job.logo} alt="" className="h-10 mb-2" />
 
-                <div>
-                    <h3>{job.title}</h3>
-                    <p>{job.company}</p>
-                    <div className="flex space-x-2">
-                        <p>{job.location}</p>
-                        <p>{job.duration}</p>
-                        <p>
-                            {job.salary.init} - {job.salary.final}
+                <div className="mb-2">
+                    <h2 className="font-bold text-2xl mb-2">{job.title}</h2>
+                    <p className="mb-2">{job.company}</p>
+                    <div className="flex space-x-3 font-extralight">
+                        <p className="bg-gray-200 p-0.5 px-2 rounded-lg">
+                            {job.location}
+                        </p>
+                        <p className="bg-gray-200 p-0.5 px-2 rounded-lg">
+                            {job.duration}
+                        </p>
+                        <p className="bg-gray-200 p-0.5 px-2 rounded-lg">
+                            ${job.salary.init} - ${job.salary.final}
                         </p>
                     </div>
                 </div>
