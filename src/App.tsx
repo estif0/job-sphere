dea5fed2162ee87fb5644347f52fed8b6d8c9852
Estifanose Sahilu <estifanoswork@gmail.com>
2025-02-05 18:29:08 +0300

    feat - Add nav bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
     return (
         <>
-            <h1 className="text-8xl text-blue-700">Test Mike 1 2 3</h1>
+            <Navbar />
         </>
     );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import companyLogo from "../assets/job-sphere-logo.png";
+
+export const Navbar = () => {
+    return (
+        <nav className="w-full px-36 py-5 flex justify-between shadow-lg shadow-gray-300 shadow-b-lg">
+            <img
+                src={companyLogo}
+                alt="Job Search Logo"
+                className="w-40 h-16 bg-blue-800 p-2 rounded-lg"
+            />
+            <div className="nav-lists  flex justify-between items-center space-x-8">
+                <a
+                    href="#"
+                    className="text-xl focus:font-bold focus:text-blue-700 focus:underline focus:underline-offset-8"
+                >
+                    Job Search
+                </a>
+                <a
+                    href="#"
+                    className="text-xl focus:font-bold focus:text-blue-700 focus:underline focus:underline-offset-8"
+                >
+                    My Applications
+                </a>
+                <a
+                    href="#"
+                    className="text-xl focus:font-bold focus:text-blue-700 focus:underline focus:underline-offset-8"
+                >
+                    Companies
+                </a>
+                <a
+                    href="#"
+                    className="text-xl focus:font-bold focus:text-blue-700 focus:underline focus:underline-offset-8"
+                >
+                    Contact Us
+                </a>
+            </div>
+            <div className="nav-buttons flex justify-between items-center space-x-4">
+                <button className="bg-blue-800 text-white px-8 h-10 rounded-lg">
+                    Login
+                </button>
+                <button className=" px-8 h-10 rounded-lg border-2 border-blue-800">
+                    Sign in
+                </button>
+            </div>
+        </nav>
+    );
+};
