d85a6b69ff18f698f1ec3462b1a30836c23c3289
Estifanose Sahilu <estifanoswork@gmail.com>
2025-02-05 19:39:03 +0300

    fix - Change the location attribute to either remote or onsite

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -11,13 +11,13 @@ export const Job: FC<JobProps> = ({ job }) => {
     return (
         // background of the job card
         <div className="mb-6 flex flex-col p-4 bg-white shadow-lg rounded-lg">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between space-x-2">
                 <img src={job.logo} alt="" className="h-20" />
 
                 <div>
                     <h3>{job.title}</h3>
                     <p>{job.company}</p>
-                    <div>
+                    <div className="flex space-x-2">
                         <p>{job.location}</p>
                         <p>{job.duration}</p>
                         <p>
@@ -25,7 +25,7 @@ export const Job: FC<JobProps> = ({ job }) => {
                         </p>
                     </div>
                 </div>
-                <div>
+                <div className="flex space-x-2">
                     <Bookmark />
                     <Share2 />
                 </div>
