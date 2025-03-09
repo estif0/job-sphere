93b8c5b84fe0e4be378b7ef726123cc1019291c4
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-09 18:38:37 +0300

    fix: Change the responsiveness of the navbar

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -1,33 +1,34 @@
 import Logo from "../../assets/icons/Logo.svg";
 import { Link } from "react-router-dom";
+
 const NavBar = () => {
     return (
         <nav className="w-full px-3 md:px-36 py-3 flex flex-wrap justify-between items-center shadow shadow-gray-300">
             <Link to="/">
                 <img src={Logo} className="h-12" alt="Logo" />
             </Link>
-            <div className="nav-lists hidden md:flex justify-between items-center space-x-10 md:space-x-6">
+            <div className="nav-lists hidden lg:flex justify-between items-center space-x-10 md:space-x-6">
                 <a
                     href="#"
-                    className=" focus:text-blue-700 focus:underline focus:underline-offset-8"
+                    className="focus:text-blue-700 focus:underline focus:underline-offset-8"
                 >
                     Job Search
                 </a>
                 <a
                     href="#"
-                    className=" focus:text-blue-700 focus:underline focus:underline-offset-8"
+                    className="focus:text-blue-700 focus:underline focus:underline-offset-8"
                 >
                     My Applications
                 </a>
                 <a
                     href="#"
-                    className=" focus:text-blue-700 focus:underline focus:underline-offset-8"
+                    className="focus:text-blue-700 focus:underline focus:underline-offset-8"
                 >
                     Companies
                 </a>
                 <a
                     href="#"
-                    className=" focus:text-blue-700 focus:underline focus:underline-offset-8"
+                    className="focus:text-blue-700 focus:underline focus:underline-offset-8"
                 >
                     Contact Us
                 </a>
