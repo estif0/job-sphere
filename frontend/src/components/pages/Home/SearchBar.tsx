d079327aec4a8bdd54ce88d54468e3a3aaa14bcc
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-09 05:15:47 +0300

    feat - Add searchbar

--- frontend/src/components/pages/Home/SearchBar.tsx
+++ frontend/src/components/pages/Home/SearchBar.tsx
@@ -1,6 +1,20 @@
 import { Search } from "lucide-react";
 const SearchBar = () => {
-    return <div className="flex justify-center"></div>;
+    return (
+        <div className="flex justify-center">
+            <form className="mt-2 w-1/3 mx-auto rounded-full h-10 relative shadow-sm shadow-gray-500">
+                <input
+                    type="text"
+                    className="w-full h-full text-gray-500 text-xs tracking-wide outline-none border border-gray-400/50 rounded-2xl px-8"
+                    placeholder="Job title, Keywords, or Company name | Location"
+                />
+                <Search className="size-4 absolute top-1/2 transform -translate-y-1/2 text-gray-800 left-3" />
+                <button className="bg-blue-700 text-white text-sm px-3 py-1 absolute top-1/2 transform -translate-y-1/2 right-2 rounded-lg cursor-pointer">
+                    Search
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default SearchBar;
