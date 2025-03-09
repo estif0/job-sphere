ba0e1bbd14b2f6c384ab83ba4528cf8df69ecfad
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-09 17:22:05 +0300

    feat: Add navigation to the home page

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -1,9 +1,11 @@
 import Logo from "../../assets/icons/Logo.svg";
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <nav className="w-full px-3 md:px-36 py-3 flex flex-wrap justify-between items-center shadow shadow-gray-300">
-            <img src={Logo} className="h-12" alt="Logo" />
+            <Link to="/">
+                <img src={Logo} className="h-12" alt="Logo" />
+            </Link>
             <div className="nav-lists hidden md:flex justify-between items-center space-x-10 md:space-x-6">
                 <a
                     href="#"
@@ -31,12 +33,18 @@ const NavBar = () => {
                 </a>
             </div>
             <div className="nav-buttons flex justify-between items-center space-x-1 md:space-x-2">
-                <button className="bg-blue-800 text-white px-8  md:px-10 h-8 rounded-lg">
+                <Link
+                    to="/login"
+                    className="bg-blue-800 text-white px-8 md:px-10 h-10 rounded-lg flex items-center justify-center"
+                >
                     Login
-                </button>
-                <button className="px-8 md:px-10 h-8 rounded-lg border-2 border-blue-800">
+                </Link>
+                <Link
+                    to="/signup"
+                    className="px-8 md:px-10 h-10 rounded-lg border-2 border-blue-800 flex items-center justify-center"
+                >
                     Sign Up
-                </button>
+                </Link>
             </div>
         </nav>
     );
