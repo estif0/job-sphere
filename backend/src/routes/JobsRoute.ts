1f6c4a2d5d80c9ebf1fcd17f7e6bd68ea76c59d6
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-11 16:59:54 +0300

    feat: Implement job validator middleware

--- backend/src/routes/JobsRoute.ts
+++ backend/src/routes/JobsRoute.ts
@@ -6,12 +6,18 @@ import {
     updateJobById,
     deleteJobById,
 } from "../controllers/JobController";
+import { handleValidationErrors, validateJob } from "../middleware/validator";
 
 const jobRouter = express.Router();
 
 jobRouter.get("/", getJobs);
 jobRouter.get("/:id", getJobById);
-jobRouter.post("/", createJob);
-jobRouter.put("/update/:id", updateJobById);
+jobRouter.post("/", validateJob, handleValidationErrors, createJob);
+jobRouter.put(
+    "/update/:id",
+    validateJob,
+    handleValidationErrors,
+    updateJobById
+);
 jobRouter.delete("/delete/:id", deleteJobById);
 export default jobRouter;
