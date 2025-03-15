98b479179ce0194cbe9eef2b7c57d93bebf95b9f
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-16 00:23:10 +0300

    fix: Reinitalized UserRoute

--- backend/src/routes/UsersRoute.ts
+++ backend/src/routes/UsersRoute.ts
@@ -1,23 +1,12 @@
-// import { Router } from "express";
-// import { protectedRoute, register, login } from "../controllers/userController";
-// import { loginValidation, registerValidation } from "../middleware/validator";
+import { Router } from "express";
 
-// const userRouter = Router();
-// import { Request, Response, NextFunction, RequestHandler } from "express";
+const userRouter = Router();
 
-// const asyncHandler = (
-//     fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
-// ): RequestHandler => {
-//     return (req, res, next) => {
-//         Promise.resolve(fn(req, res, next)).catch(next);
-//     };
-// };
+userRouter.post("/login");
 
-// userRouter.post("/login", loginValidation(), asyncHandler(login));
+userRouter.post("/register");
 
-// userRouter.post("/register", registerValidation(), asyncHandler(register));
+userRouter.get("/protected");
+userRouter.get("/protected-admin");
 
-// userRouter.get("/protected", asyncHandler(protectedRoute));
-// userRouter.get("/protected-admin", asyncHandler(protectedRoute));
-
-// export default userRouter;
+export default userRouter;
