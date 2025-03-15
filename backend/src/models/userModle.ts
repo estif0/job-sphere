e795d63efd1f69fb11719aa4c766ae0c9edb76de
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-15 07:44:39 +0300

    feat: Add user model

---
+++ backend/src/models/userModle.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+    role: {
+        type: String,
+        required: true,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+});
+export default mongoose.model("User", userSchema);
