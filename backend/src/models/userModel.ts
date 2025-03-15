c35c67e62fd8e333fc7e0688975b4bc31e1ec727
Estifanose Sahilu <estifanoswork@gmail.com>
2025-03-16 00:20:30 +0300

    fix: Update use model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,29 +1,40 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
 
-    role: {
-        type: String,
-        required: true,
-        enum: ["USER", "ADMIN"],
-        default: "USER",
+        role: {
+            type: String,
+            required: true,
+            enum: ["USER", "ADMIN"],
+            default: "USER",
+        },
+        bookmarkedJobs: {
+            type: Array,
+            default: [],
+        },
+        appliedJobs: {
+            type: Array,
+            default: [],
+        },
     },
-});
+    { timestamps: true }
+);
 export default mongoose.model("User", userSchema);
